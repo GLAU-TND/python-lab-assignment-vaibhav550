8da445268cb88388d50a2492ec9ac302e08933e7
vaibhav550 <54625320+vaibhav550@users.noreply.github.com>
2019-11-30 16:25:44 +0530

    Update solution2.py

--- solution2.py
+++ solution2.py
@@ -1,15 +1,19 @@
 c=(2,1,3,4,5,6)
 try:
-    #c.remove(5)
-    
-    #c=5+'sj'
-    
-    #c=float('hello')
-
+    c.remove(5)
 except AttributeError as e:
     print("AttributeError",e)
+else:
+    print("Process 1")
+try:
+    c=5+'str'
 except TypeError as e:
     print("TypeError",e)
+else:
+    print("Process 2")
+
+try:
+    c=float('Hello')
 except ValueError as e:
     print("ValueError",e)
 finally:
